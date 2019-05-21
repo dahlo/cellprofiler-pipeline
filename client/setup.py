42457f844af1be4f8274cae89f357ca7de0a867c
Ben Blamey <blamey.ben@gmail.com>
2019-05-21 13:11:40 +0200

    fix pkg name

--- client/setup.py
+++ client/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(name='haste_pipeline_client',
       version='0.10',
-      packages=['haste.pipeline.server'],
+      packages=['haste.pipeline.client'],
       namespace_packages=[
           'haste',
           'haste.pipeline',
