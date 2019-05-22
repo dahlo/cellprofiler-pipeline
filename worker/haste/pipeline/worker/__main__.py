47cbcc5bc9cc32eb1a8c8423b0e8bdadf92ccba5
Ben Blamey <blamey.ben@gmail.com>
2019-05-22 15:45:25 +0200

    print config

--- worker/haste/pipeline/worker/__main__.py
+++ worker/haste/pipeline/worker/__main__.py
@@ -1,10 +1,16 @@
 import argparse
+import logging
 
 import pika
 from pika import PlainCredentials
 from sys import argv
 
 ARG_PARSE_PROG_NAME = 'python3 -u -m haste.pipeline.worker'
+PAUSE_SECS = 5
+
+LOGGING_LEVEL = logging.INFO
+LOGGING_FORMAT_DATE = '%Y-%m-%d %H:%M:%S.%d3'
+LOGGING_FORMAT = '%(asctime)s - AGENT - %(threadName)s - %(levelname)s - %(message)s'
 
 
 def parse_args():
@@ -13,15 +19,16 @@ def parse_args():
 
     # parser.add_argument('--include', type=str, nargs='?', help='include only files with this extension')
     # parser.add_argument('--tag', type=str, nargs='?', help='short ASCII tag to identify this machine (e.g. ben-laptop)')
-    parser.add_argument('--host', type=str, nargs='?', help='Hostname for RabbitMQ', default='localhost')
+    parser.add_argument('--host', type=str, nargs='?', help='Hostname for AMPQ (eg. RabbitMQ)', default='localhost')
     parser.add_argument('--username', type=str, nargs='?', help='Username for AMPQ', default='guest')
     parser.add_argument('--password', type=str, nargs='?', help='Password for AMPQ', default='guest')
+    parser.add_argument('--config', type=str, nargs='?', help='configuration in JSON', default='')
     #
     # parser.add_argument('--x-preprocessing-cores', default=1, type=int)
     # parser.add_argument('--x-mode', default=1, type=int)
     # parser.set_defaults(prio=True)
 
-    parser.add_argument('path', metavar='path', type=str, nargs=1, help='path to watch (e.g. C:/docs/foo')
+    parser.add_argument('path', metavar='path', type=str, nargs=1, help='path to watch (e.g. C:/docs/foo)')
 
     args = parser.parse_args()
     return args
@@ -29,7 +36,16 @@ def parse_args():
 
 
 def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
+    # ch: blocking channel
+    # body -- in bytes
+    # properties -- BasicProperties   call .headers -> dict
+
+    body = body.decode('utf-8') # TODO: hmmm... maybe this is ASCII? nm.
+    headers = properties.headers
+
+    # print(" [x] Received %r " % body)
+
+    logging.info(f'received: {body} {headers}')
 
 
 if __name__ == '__main__':
@@ -38,10 +54,17 @@ if __name__ == '__main__':
     path = args.path
     # include = args.include
     # tag = args.tag
-    cli_rabbitmq_host = args.host
+    ampq_host = args.host
+
+    config = args.config
+
+    logging.info(f'starting with args {[path, ampq_host, config]} (excl. creds)')
+
+
+
 
     connection = pika.BlockingConnection(
-        pika.ConnectionParameters(cli_rabbitmq_host, credentials=PlainCredentials(args.username, args.password)))
+        pika.ConnectionParameters(ampq_host, credentials=PlainCredentials(args.username, args.password)))
     channel = connection.channel()
 
     channel.queue_declare(queue='files')
