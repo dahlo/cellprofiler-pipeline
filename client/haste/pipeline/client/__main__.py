fe712f8e063f1105b50eb78ceb430ebca7bc4cf0
dahlo <m.dahlo@gmail.com>
2019-11-11 13:28:27 +0100

    added alternative way of monitoring file system using watchdog

--- client/haste/pipeline/client/__main__.py
+++ client/haste/pipeline/client/__main__.py
@@ -7,6 +7,10 @@ import time
 from pika import PlainCredentials
 from sys import argv
 
+from watchdog.observers import Observer
+from watchdog.observers.polling import PollingObserver
+from watchdog.events import PatternMatchingEventHandler
+
 ARG_PARSE_PROG_NAME = 'python3 -u -m haste.pipeline.client'
 PAUSE_SECS = 5
 
@@ -25,8 +29,8 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Watch directory and stream new files to HASTE',
                                      prog=ARG_PARSE_PROG_NAME)
 
-    parser.add_argument('--include', type=str, nargs='?', help='include only files with this extension')
-    parser.add_argument('--tag', type=str, nargs='?', help='short ASCII tag to identify this machine (e.g. ben-laptop)')
+    parser.add_argument('--include', type=str, nargs='?', help='include only files with this extension(s). Comma separated if multiple. Ex: .jpg,.jpeg,.png', default=['*'])
+    parser.add_argument('--tag', type=str, nargs='?', help='short ASCII tag to identify this machine (e.g. ben-laptop)', default="")
     parser.add_argument('--host', type=str, nargs='?', help='Hostname for RabbitMQ', default='localhost')
     parser.add_argument('--username', type=str, nargs='?', help='Username for AMPQ', default='guest')
     parser.add_argument('--password', type=str, nargs='?', help='Password for AMPQ', default='guest')
@@ -39,85 +43,135 @@ def parse_args():
 
     args = parser.parse_args()
     return args
+    
+    
+# file watcher events
+# def on_deleted(event):
+# def on_modified(event):
+# def on_moved(event):
+def on_created(event):
+    
+    # wait until the file size has stopped changing
+    # file_size = -1
+    # while file_size != os.path.getsize(event.src_path):
+        # file_size = os.path.getsize(event.src_path)
+        # time.sleep(1)
+        
+    global filenames_previous, args
+        
+    # skip if the file has been seen before
+    if event.src_path in filenames_previous:
+        return
+        
+    body = event.src_path
+
+    # TODO: some encoding issue here?! -- use strings
+    hdrs = {"tag": tag,
+            "timestamp": str(time.time()),
+            "path": str(path),
+            "stream_id": stream_id}
+    properties = pika.BasicProperties(app_id='haste.pipeline.client',
+                                      content_type='application/json',
+                                      headers=hdrs)
+    
+    # publish the message
+    publish_rmq(queue='files', properties=properties, body=body)
+                          
+    # remember the file
+    filenames_previous.add(event.src_path)
+    logging.info(f"Sent {body}")
+    
+    
+# fuction to publish to RabbitMQ that will try to reconnect to the mq if timedout
+def publish_rmq(queue, properties, body, exchange=''):
+    
+    global connection, channel
+        
+    try:
+        channel.basic_publish(exchange=exchange,
+                              routing_key=queue,
+                              body=body,
+                              properties=properties)
+        
+    # try reconnecting if the connection has timedout
+    except pika.exceptions.StreamLostError:
+        
+        # reconnect
+        logging.info(f'Reconneting to RabbitMQ server: {args.host} (excl. creds)')
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(args.host, credentials=PlainCredentials(args.username, args.password), heartbeat=0))
+        channel = connection.channel()
+        channel.queue_declare(queue=queue)
+        
+        # try publishing again
+        channel.basic_publish(exchange=exchange,
+                              routing_key=queue,
+                              body=body,
+                              properties=properties)
+
+
+
+# a dummy object to be able to trick the event manager that a file has been triggered
+class DummyEvent():
+    pass
 
 
 def main():
     logging.basicConfig(level=LOGGING_LEVEL,
                         format=LOGGING_FORMAT,
                         datefmt=LOGGING_FORMAT_DATE)
-
-    global include, filenames_previous
+                        
+    # readability
+    global path, tag, channel, stream_id, filenames_previous, connection, channel, args
     args = parse_args()
     path = args.path[0]
-    include = args.include
-    if not include.startswith('.'):
-        include = '.' + include
     tag = args.tag
     rabbitmq_host = args.host
-
     stream_id = create_stream_id(tag)
-
+    filenames_previous = set()
+               
+    # if multiple file endings
+    include = args.include.split(',')
+    
+    # add star before the file ending if missing
+    include = [ ext.lower() if ext.startswith('*') else "*"+ext.lower() for ext in include ]
+    
     logging.info(f'starting with args {[path, include, tag, rabbitmq_host]} (excl. creds)')
-
+    
+    # create the event handler
+    my_event_handler = PatternMatchingEventHandler(include, ignore_patterns="", ignore_directories=True, case_sensitive=False)
+    my_event_handler.on_created = on_created
+    # my_observer = Observer() # only on local file systems (inotify)
+    my_observer = PollingObserver(timeout=20)
+    my_observer.schedule(my_event_handler, path, recursive=False)
+    
     # TODO: make queue persistent between rabbitMQ restarts?
-
+    # connect to the RabbitMQ server
+    logging.info(f'starting with args {[path, include, tag, rabbitmq_host]} (excl. creds)')
     connection = pika.BlockingConnection(
-        pika.ConnectionParameters(rabbitmq_host, credentials=PlainCredentials(args.username, args.password)))
+        pika.ConnectionParameters(args.host, credentials=PlainCredentials(args.username, args.password), heartbeat=0))
     channel = connection.channel()
     channel.queue_declare(queue='files')
 
     logging.info('connected to AMPQ.')
-
-
+    
+    # trigger event for all existing files?
+    existing_file_event = DummyEvent()
+    for existing_file in os.listdir(path): # list dir
+        if os.path.isfile(os.path.join(path, existing_file)): # only files
+            if f'*{os.path.splitext(existing_file)[-1].lower()}' in include: # if extension is matching            
+                existing_file_event.src_path = os.path.join(path, existing_file) # add property to dummy object to have the same structure as a watchdog even object
+                on_created(existing_file_event) # manually trigger creation function
+    
+    # start monitoring
+    my_observer.start()
+    logging.info('started monitoring directory.')
     try:
-        filenames_previous = set()
-
         while True:
-            time_start = time.time()
-
-            logging.info(f'starting dir listing of {path}')
-
-            filenames = os.listdir(path)
-
-            filenames = list(filter(lambda filename: filename.endswith(include), filenames))
-
-            logging.debug(filenames)
-
-            filenames_after_filter = list(filter(lambda filename: filename not in filenames_previous, filenames))
-
-            if len(filenames) == 0:
-                logging.info('no new files found.')
-
-            for filename in filenames_after_filter:
-                # body = f'Hello World! {time.time()}'
-                body = filename
-
-                # TODO: some encoding issue here?! -- use strings
-                hdrs = {"tag": tag,
-                        "timestamp": str(time_start),
-                        "path": str(path),
-                        "stream_id": stream_id}
-                properties = pika.BasicProperties(app_id='haste.pipeline.client',
-                                                  content_type='application/json',
-                                                  headers=hdrs)
-
-
-                channel.basic_publish(exchange='',
-                                      routing_key='files',
-                                      body=body,
-                                      properties=properties)
-                logging.info(f"Sent {body}")
-
-            pause = (time_start + 5) - time.time()
-
-            if pause < 0:
-                logging.warn(f'dir listing overran by {-pause} seconds')
-            else:
-                time.sleep(pause)
-
-            filenames_previous = set(filenames)
-
-    finally:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        my_observer.stop()
+        my_observer.join()
         connection.close()
 
 
